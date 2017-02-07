b1196695d3d4b153df318631689718c325bc1f15
Petru (Armand) Bancila <petru.bancila@kcl.ac.uk>
2017-02-07 20:58:40 +0000

    Add Twitter button functionality

--- js/index.js
+++ js/index.js
@@ -1,29 +1,33 @@
 // this will be sent to the twitter link
 // TODO: modify twitter link to make it include this data
-var quote = "";
-var author = "";
+function updateTwitterLink(quote, author) {
+  $("#twitter-link").attr("href", "http://www.google.com");
+  $("#twitter-link").attr("href", "https://twitter.com/intent/tweet?hashtags=quotes&text=" + encodeURIComponent('"' + quote + '" -' + author));
+}
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "y7zVXhDu30mshkawllC8uoBq4NVUp1r0BAMjsnhk5mwZwWgIW4"
+      , Accept: "application/json"
+      , "Content-Type": "application/x-www-form-urlencoded"
+    }
+    , url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat='
+    , success: function (data) {
+      var json = JSON.parse(data);
+      var quote = json.quote;
+      var author = json.author;
+      $("#quote-text").text(quote);
+      $("#author-text").text(author);
+      updateTwitterLink(quote, author);
+    }
+  });
+  // update twitter link
+}
 // js function that gets the quote text
 // the squery thing will modify content and call js function
-$(document).ready(function() {
-  $("#get-quote").on("click", function() {
-    $.ajax({
-        headers: {
-          "X-Mashape-Key": "y7zVXhDu30mshkawllC8uoBq4NVUp1r0BAMjsnhk5mwZwWgIW4",
-          Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-        success: function(data) {
-          var json = JSON.parse(data);
-          $("#quote-text").text(json.quote);
-          $("#quote-author").text(json.author);
-        }
-    });
-    // update twitter link
-  });
-  
-  $("twitter-button").on("click", function() {
-    
-  });
-  
+$(document).ready(function () {
+  getQuote();
+  $("#get-quote").on("click", getQuote);
+  // $("twitter-button").on("click", getQuote());
 });
